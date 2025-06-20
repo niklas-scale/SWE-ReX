4c68aba5ae337969a52b647ad2cd360a0657522f
Maya Krishnan <maya.krishnan@scale.com>
2025-06-20 15:23:04 +0000

    Update swe-rex patch with new changes to support SWEAP instances

--- src/swerex/deployment/modal.py
+++ src/swerex/deployment/modal.py
@@ -67,23 +67,28 @@ class _ImageBuilder:
     def from_ecr(self, image: str) -> modal.Image:
         self.logger.info(f"Building image from ECR {image}")
         try:
-            # Build image for SWEAP instances
-            # TODO: Expand for more use cases rather than just SWEAP
-            # TODO: Move AWS Secret from a username specific name to a generic one
             return modal.Image.from_aws_ecr(  # type: ignore
                 image,
-                secret=modal.Secret.from_name("aws-secret-ml-xiang-deng"),
+                secret=modal.Secret.from_name(os.environ.get("MODAL_AWS_SECRET_NAME", "aws-secret-ml-xiang-deng")),
+                setup_dockerfile_commands=["RUN apt update && apt install -y pip || true", "RUN python -m pip config set global.break-system-packages true || true"]
             )
         except NoCredentialsError as e:
             msg = "AWS credentials not found. Please configure your AWS credentials."
             raise ValueError(msg) from e
 
     def ensure_pipx_installed(self, image: modal.Image) -> modal.Image:
+
         image = image\
             .run_commands("pip config unset global.index-url || true") \
-            .apt_install("pipx") \
-            .run_commands(f"pipx install {PACKAGE_NAME}")
-        return image.run_commands("pipx ensurepath")
+            .run_commands("apt update && apt install -y curl") \
+            .run_commands("curl https://pyenv.run | bash") \
+            .run_commands("apt update && DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get -y install tzdata && apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl git libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev") \
+            .run_commands("~/.pyenv/bin/pyenv install 3.11.13") \
+            .run_commands("~/.pyenv/versions/3.11.13/bin/python3.11 -m pip install pipx") \
+            .run_commands("~/.pyenv/versions/3.11.13/bin/python3.11 -m pipx ensurepath") \
+            .entrypoint([])
+
+        return image
 
     def auto(self, image_spec: str | modal.Image | PurePath) -> modal.Image:
         if isinstance(image_spec, modal.Image):
@@ -114,10 +119,10 @@ class ModalDeployment(AbstractDeployment):
         logger: logging.Logger | None = None,
         image: str | modal.Image | PurePath,
         startup_timeout: float = 0.4,
-        runtime_timeout: float = 3600.0,
+        runtime_timeout: float = 1800.0,
         modal_sandbox_kwargs: dict[str, Any] | None = None,
         install_pipx: bool = True,
-        deployment_timeout: float = 3600.0,
+        deployment_timeout: float = 1800.0,
     ):
         """Deployment for modal.com. The deployment will only start when the
         `start` method is being called.
@@ -191,7 +196,7 @@ class ModalDeployment(AbstractDeployment):
         install pipx and then run swerex-server with pipx run
         """
         rex_args = f"--port {self._port} --auth-token {token}"
-        return f"{REMOTE_EXECUTABLE_NAME} {rex_args} || pipx run {PACKAGE_NAME} {rex_args}"
+        return f"{REMOTE_EXECUTABLE_NAME} {rex_args} || ~/.pyenv/versions/3.11.13/bin/python3.11 -m pipx run {PACKAGE_NAME} {rex_args}"
 
     def get_modal_log_url(self) -> str:
         """Returns URL to modal logs
